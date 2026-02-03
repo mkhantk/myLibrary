831df65d865cdadf7a1f5d9396b0aa0daa0a9cef
Min Khant Kyaw <mkhantk102@gmail.com>
2026-02-04 02:09:03 +0630

    change to class

--- src/components/constant.ts
+++ src/components/constant.ts
@@ -2,32 +2,50 @@ import { useState } from "react";
 import { v4 } from "uuid";
 
 type BookInstance = {
-	id: any;
+	id?: any;
 	title: string;
 	author: string;
 	pages: string;
 	status: string;
-	updateStatus: (status: string) => void;
+	updateStatus?: (status: string) => void;
 };
 
-function Book(
-	this: BookInstance,
-	title: string,
-	author: string,
-	pages: string,
-	status: string,
-) {
-	if (!new.target) throw new Error("something went wrong, please try again.");
-	this.id = v4();
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.status = status;
+class Book {
+	id: string;
+	title: string;
+	author: string;
+	pages: string;
+	status: string;
+
+	constructor({ title, author, pages, status }: BookInstance) {
+		this.id = v4();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.status = status;
+	}
+	updateStatus(status: string) {
+		this.status = status;
+	}
 }
+// function Book(
+// 	this: BookInstance,
+// 	title: string,
+// 	author: string,
+// 	pages: string,
+// 	status: string,
+// ) {
+// 	if (!new.target) throw new Error("something went wrong, please try again.");
+// 	this.id = v4();
+// 	this.title = title;
+// 	this.author = author;
+// 	this.pages = pages;
+// 	this.status = status;
+// }
 
-Book.prototype.updateStatus = function (this: BookInstance, status: string) {
-	this.status = status;
-};
+// Book.prototype.updateStatus = function (this: BookInstance, status: string) {
+// 	this.status = status;
+// };
 
 export default function useLibrary() {
 	const [book, setBook] = useState<BookInstance[]>([]);
@@ -38,14 +56,14 @@ export default function useLibrary() {
 		status: string,
 	) {
 		// funciotn for adding new books to the data
-		const newBook = new (Book as any)(title, author, pages, status);
+		const newBook = new Book({ title, author, pages, status });
 		setBook((prev) => [...prev, newBook]);
 	}
 	function changeStatus(id: string, status: string) {
 		setBook((prev) =>
 			prev.map((item) => {
 				if (item.id === id) {
-					item.updateStatus(status);
+					item.updateStatus?.(status);
 				}
 				return item;
 			}),
